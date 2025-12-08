3b3f854974415d99abbc1e0b54a181fbf30e9531
sathiyan14 <sathiyanp4@gmail.com>
2025-12-08 11:12:25 +0530

    feat: integrate Bootstrap and Sass for improved styling

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,11 @@
 use App\Http\Controllers\Usercontroller;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/hello', [Usercontroller::class, 'hello']);
+Route::get('/', [Usercontroller::class, 'hello']);
+
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
